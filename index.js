b5ea044c2f28f8a61b90c63f2e4c2de00dd7b7cd
Victor Hugo <frontdevvictor@gmail.com>
2025-09-04 11:30:27 -0300

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ new Vue({
   el: "#app",
   data() {
     return {
-      currentCardBackground: Math.floor(Math.random()* 25 + 1), // just for fun :D
+      currentCardBackground: Math.floor(Math.random() * 25 + 1),
       cardName: "",
       cardNumber: "",
       cardMonth: "",
@@ -11,73 +11,62 @@ new Vue({
       minCardYear: new Date().getFullYear(),
       amexCardMask: "#### ###### #####",
       otherCardMask: "#### #### #### ####",
-      cardNumberTemp: "",
       isCardFlipped: false,
       focusElementStyle: null,
       isInputFocused: false
     };
   },
   mounted() {
-    this.cardNumberTemp = this.otherCardMask;
     document.getElementById("cardNumber").focus();
   },
   computed: {
-    getCardType () {
-      let number = this.cardNumber;
-      let re = new RegExp("^4");
-      if (number.match(re) != null) return "visa";
-
-      re = new RegExp("^(34|37)");
-      if (number.match(re) != null) return "amex";
-
-      re = new RegExp("^5[1-5]");
-      if (number.match(re) != null) return "mastercard";
-
-      re = new RegExp("^6011");
-      if (number.match(re) != null) return "discover";
-      
-      re = new RegExp('^9792')
-      if (number.match(re) != null) return 'troy'
-
-      return "visa"; // default type
+    getCardType() {
+      const number = this.cardNumber;
+      if (/^4/.test(number)) return "visa";
+      if (/^(34|37)/.test(number)) return "amex";
+      if (/^5[1-5]/.test(number)) return "mastercard";
+      if (/^6011/.test(number)) return "discover";
+      if (/^9792/.test(number)) return "troy";
+      return "unknown";
     },
-        generateCardNumberMask () {
-            return this.getCardType === "amex" ? this.amexCardMask : this.otherCardMask;
+    generateCardNumberMask() {
+      return this.getCardType === "amex" ? this.amexCardMask : this.otherCardMask;
     },
-    minCardMonth () {
-      if (this.cardYear === this.minCardYear) return new Date().getMonth() + 1;
+    minCardMonth() {
+      if (this.cardYear == this.minCardYear) return new Date().getMonth() + 1;
       return 1;
     }
   },
   watch: {
-    cardYear () {
+    cardYear() {
       if (this.cardMonth < this.minCardMonth) {
         this.cardMonth = "";
       }
     }
   },
   methods: {
-    flipCard (status) {
+    flipCard(status) {
       this.isCardFlipped = status;
     },
-    focusInput (e) {
+    focusInput(e) {
       this.isInputFocused = true;
       let targetRef = e.target.dataset.ref;
       let target = this.$refs[targetRef];
-      this.focusElementStyle = {
-        width: `${target.offsetWidth}px`,
-        height: `${target.offsetHeight}px`,
-        transform: `translateX(${target.offsetLeft}px) translateY(${target.offsetTop}px)`
+      if (target) {
+        this.focusElementStyle = {
+          width: `${target.offsetWidth}px`,
+          height: `${target.offsetHeight}px`,
+          transform: `translateX(${target.offsetLeft}px) translateY(${target.offsetTop}px)`
+        };
       }
     },
     blurInput() {
-      let vm = this;
+      this.isInputFocused = false;
       setTimeout(() => {
-        if (!vm.isInputFocused) {
-          vm.focusElementStyle = null;
+        if (!this.isInputFocused) {
+          this.focusElementStyle = null;
         }
       }, 300);
-      vm.isInputFocused = false;
     }
   }
 });
